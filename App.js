de1bb6011a3df533e3c803afb0a9daa3884d99b2
sultonza24 <6110110107@psu.ac.th>
2020-08-30 18:25:39 +0700

    commit 8

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from 'react'
+
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ZipCodeScreen from './screen/ZipCodeScreen';
 import WeatherScreen from './screen/WeatherScreen';
+import { StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
+const Stack = createStackNavigator()
+
+export default function App(){
+  return(
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={ZipCodeScreen}/>
-        <Stack.Screen name="Weather" component={WeatherScreen}/>
+        <Stack.Screen name="Home" component={ZipCodeScreen} style={styles.backdrop1} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />      
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
+const styles = StyleSheet.create({
+  backdrop1: {
+    flexDirection: 'column', //Default Portrait
+    justifyContent: 'center',
+    alignItems: 'center', // opposite of FlexDirection is lanscape
+}
+})
