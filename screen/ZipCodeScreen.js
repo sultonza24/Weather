7ae1d7c8fe8f853962ab6a8d50f3d60a06c4172d
sultonza24 <6110110107@psu.ac.th>
2020-08-30 18:24:48 +0700

    commit 7

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,48 +1,81 @@
 import React from 'react'
-import { FlatList, View, Text, TouchableHighlight, StyleSheet } from 'react-native'
+import { FlatList, View, Text, StyleSheet, ImageBackground, Linking } from 'react-native'
+import { TouchableHighlight } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
 
-const availableZipItems = [
-    { place: 'Hatyai', code: '90110' },
-    { place: 'Trang', code: '92000' },
-    { place: 'Chiangmai', code: '50000' },
-    { place: 'Khonkaen', code: '40000' },
-    { place: 'Chonburi', code: '20000' },
+const AvailableZipItems = [
+    { place: 'Chiangmai', code: '50000'},
+    { place: 'Chonburi', code: '20000'},
+    { place: 'Hatyai', code: '90110'},
+    { place: 'Khonkaen', code: '40000'},
+    { place: 'Trang', code: '92000'},
+    { place: 'Pattani', code: '94000'},
+    { place: 'Saiburi', code: '94110'},
+    { place: 'Panare', code: '94130'},
+       
 ]
-   
 
 const ZipItem = ({place, code, navigation}) => (
     <TouchableHighlight onPress={() => {
         navigation.navigate('Weather', {zipCode: code})
     }}>
-        <View style={styles.zipCode}>
-            <Text>{place}</Text>
-            <Text>{code}</Text>
+        <View style={styles.zipItem}>
+            <Text style={styles.zipPlace}>{place}</Text>
+            <Text style={styles.zipCode}>{code}</Text>
         </View>
     </TouchableHighlight>
+
 )
 
-export default function ZipCodeScreen(){
+export default function ZipCodeScreen() {
     const navigation = useNavigation()
     return (
-        <FlatList
-            data = {availableZipItems}
-            keyExtractor = {item => item.code}
-            renderItem = {({item}) => <ZipItem {...item}/>}
-        />
+        <ImageBackground source={require('../image/background.jpg')} style={styles.backdrop}>
+          <View style={{
+            flex: 1,
+            flexDirection: 'row',
+        }}>
+            <FlatList
+                data = {AvailableZipItems}
+                keyExtractor = {item =>item.code}
+                renderItem = {({item}) => <ZipItem {...item} navigation={navigation}/>}
+            />
+          </View>
+        </ImageBackground> 
     )
 }
 
-
 const styles = StyleSheet.create({
-    ZipItem: {
+    backdrop: {
         flex: 1,
-        flexDirection: 'row'
+        width: '100%',
+        height: '100%',
+        opacity: 0.9,
     },
+
+    zipItem: {
+        flex: 1,
+        width: '100%',
+        height: '10%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignSelf: 'auto',
+    },
+
     zipPlace: {
-        flex: 1,
+        color: '#000000',  
+        fontSize: 20,
+        paddingTop: '5%',
+        paddingBottom: '5%',
+        paddingLeft: '20%', 
     },
+
     zipCode: {
-        flex: 1,
-    }
+        color: '#000000',  
+        fontSize: 20,
+        paddingTop: '5%',
+        paddingBottom: '5%',
+        paddingRight: '20%',
+    },
+
 })
